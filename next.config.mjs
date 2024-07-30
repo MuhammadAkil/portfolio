e4b5bc8a6bdce80e5fe685eb8eef593c1c925dc6
Akil <akil.siddique@postex.pk>
2024-07-30 17:23:05 +0500

    deploy on pags

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Set your base path relative to the repository
+    basePath: '/MuhammadAkil/portfolio',
+    assetPrefix: '/MuhammadAkil/portfolio/',
+    
+    // Function to map paths for static export
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId }
+    ) {
+      return {
+        '/': { page: '/' },
+        // Define other paths if needed
+      }
+    },
+  };
+  
+  export default nextConfig;
+  
